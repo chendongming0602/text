1ded7414ee81cb9b54db0a5f294ef755fd1c2102
陈栋明 <chendm0602@163.com>
2020-01-15 16:44:59 +0800

    测试跳转另一个小程序

--- miniprogram/components/other/other.js
+++ miniprogram/components/other/other.js
@@ -11,13 +11,32 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    isShow:true
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    dot(){
+      wx.navigateToMiniProgram({
+        appId: 'wx1a15fc0525cd4706',
+        path: 'pages/index/index',
+        extraData: {
+          foo: 'bar'
+        },
+        envVersion: 'develop',
+        success(res) {
+          console.log("跳转成功")
+          // 打开成功
+        },
+        fail:err=>{
+          console.log("跳转失败",err)
+        }
+      })
+    },
+    closeRed(){
+      this.setData({isShow:true})
+    }
   }
 })

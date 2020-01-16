6ef2ae80d2bc49afd467007db6ed101424f53c5f
陈栋明 <chendm0602@163.com>
2020-01-16 19:04:07 +0800

    测试跳转小程序

--- miniprogram/pages/index/indexTab1/indexTab1.js
+++ miniprogram/pages/index/indexTab1/indexTab1.js
@@ -33,70 +33,87 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    moreList() {//更多
-      let data=this.data
+      moreList() {//更多
+        let data=this.data
+        return APP.request({
+          path: "/api/portal/articles/index",
+          method:"POST",
+          data:{
+            page:this.page,
+            type:"more"
+          }
+        }).then(res => {
+          if(res.length<20){
+            this.setData({ isEmptyList:false})
+          }
+          this.setData({
+            list: this.more ? data.list.concat(res):res
+          });
+          console.log(res)
+        });
+    },
+    newList(){//最新
       return APP.request({
         path: "/api/portal/articles/index",
-        method:"POST",
-        data:{
-          page:this.page,
-          type:"more"
+        method: "POST",
+        data: {
+          page:1,
+          type:"new"
         }
       }).then(res => {
-        if(res.length<20){
-          this.setData({ isEmptyList:false})
-        }
         this.setData({
-          list: this.more ? data.list.concat(res):res
+          newList: res
         });
-        console.log(res)
-      });
-  },
-  newList(){//最新
-    return APP.request({
-      path: "/api/portal/articles/index",
-      method: "POST",
-      data: {
-        page:1,
-        type:"new"
-      }
-    }).then(res => {
+      })
+    },
+    onPullDown(e) {//下拉刷新
+      APP.loadShow()
+      const { stop } = e.detail;
       this.setData({
-        newList: res
+        isEmptyList:true,
       });
-    })
-  },
-  onPullDown(e) {//下拉刷新
-    APP.loadShow()
-    const { stop } = e.detail;
-    this.setData({
-      isEmptyList:true,
-    });
-    this.more = false;
-    this.page=1;
-    Promise.all([this.moreList(), this.newList()])
-      .then(() => {
+      this.more = false;
+      this.page=1;
+      Promise.all([this.moreList(), this.newList()])
+        .then(() => {
+          wx.hideLoading();
+          stop();
+        }).catch(err => {
+          stop();
+          APP.hintShow("刷新失败！！")
+        });
+    },
+    onReachBottom(e){
+      const { stop } = e.detail;
+      if (!this.data.isEmptyList) return stop();
+      APP.loadShow();
+      this.more=true;
+      this.page+=1 
+      this.moreList().then(()=>{
+        stop();
+        wx.hideLoading()
+      }).catch(()=>{
         wx.hideLoading();
         stop();
-      }).catch(err => {
-        stop();
-        APP.hintShow("刷新失败！！")
-      });
-  },
-  onReachBottom(e){
-    const { stop } = e.detail;
-    if (!this.data.isEmptyList) return stop();
-    APP.loadShow();
-    this.more=true;
-    this.page+=1 
-    this.moreList().then(()=>{
-      stop();
-      wx.hideLoading()
-    }).catch(()=>{
-      wx.hideLoading();
-      stop();
-      APP.hintShow("上拉加载失败！")
-    })
-  }
+        APP.hintShow("上拉加载失败！")
+      })
+    },
+    dot() {
+      wx.navigateToMiniProgram({
+        appId: 'wx1a15fc0525cd4706',
+        path: 'pages/index/index',
+        extraData: {
+          foo: 'bar'
+        },
+        envVersion: 'trial',
+        success(res) {
+          console.log("跳转成功")
+          // 打开成功
+        },
+        fail: err => {
+          console.log("跳转失败", err)
+        }
+      })
+    },
   }
 })

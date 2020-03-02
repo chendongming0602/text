f0ac5704680bc8c9a69260a894505c155ce22e35
chendongming <chendm0602@163.com>
2020-03-02 19:09:28 +0800

    gif更换

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,7 +1,7 @@
 export const config = {
   appid: "wx660d6918c3228fab",
   apiHost: "https://article.silver.yazai.com",
-  version: "v1.2.2",
+  version: "v1.2.3",
   deviceType: "wxapp"
 }
 // 更改版本配置，设置审核信息
